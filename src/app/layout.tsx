27991fdc961ad49681c49a35c8cf47a6d53994fb
Coadal Virtualwave <coadalvirtualwave@gmail.com>
2026-09-09 13:46:24 +0530

    update: pixel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -52,7 +52,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           t.src=v;s=b.getElementsByTagName(e)[0];
           s.parentNode.insertBefore(t,s)}(window, document,'script',
           'https://connect.facebook.net/en_US/fbevents.js');
-          fbq('init', '846802607866332');
+          fbq('init', '2179999276273012');
           fbq('track', 'PageView');`}
         </Script>
         <noscript>
@@ -60,7 +60,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             height="1"
             width="1"
             style={{ display: "none" }}
-            src="https://www.facebook.com/tr?id=846802607866332&ev=PageView&noscript=1"
+            src="https://www.facebook.com/tr?id=2179999276273012&ev=PageView&noscript=1"
             alt=""
           />
         </noscript>
